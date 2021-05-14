1c34ed70c6f8d6d54da14ac359af480d3c28beb9
F_Sur <f_sur@F-Surs-MacBook-Pro.local>
2021-05-14 18:02:35 +0800

    Initial Project

---
+++ EducationalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EducationalApp
+//
+//  Created by F_Sur on 14/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
